a99238c813bf0dec1c69b5ad48555e7790e0b993
zhangmingyuan <2369558390@qq.com>
2023-10-24 20:50:03 +0800

    feat: 脚手架first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="index">
+    <Counter />
+    <div>
+      啊哈哈
+    </div>
+  </view>
+</template>
+
+<script>
+import './index.less'
+import Counter from '../../components/Counter.vue'
+
+export default {
+  name: 'AbC',
+  components: {
+    Counter
+  }
+}
+</script>
